95a8baf0bf0b1b13d991a40b6f51bc138c33bd72
natii <medaigualqueseafacil@gmail.com>
2021-08-25 18:28:32 -0300

    archivo html de register

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path = require('path');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
-
+app.get( '/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+app.get( '/register', (req, res) => res.sendFile(path.join(__dirname, 'views', 'register.html')));
 
 
 
